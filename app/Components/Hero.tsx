597f489007985a47743fafd0e0909895151e5b3a
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-07-26 02:29:28 +0500

    added NavBar

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -1,17 +1,54 @@
 "use client"
 import {motion} from 'framer-motion'
+import Link from 'next/link';
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+
+const icons = [{
+    url:"https://www.linkedin.com/in/syedshahmirsultan/",
+    icon:<FaLinkedinIn size={23}/>
+},
+{
+    url:"https://github.com/syedshahmirsultan",
+    icon:<FaGithub size={23}/>
+},
+{
+    url:"https://www.facebook.com/profile.php?id=100093155077830",
+    icon:<FaFacebookF size={23}/>
+}
+]
+
 const Hero = () => {
     return (
-        <div className='flex flex-col gap-y-3 pt-40 md:pt-0 justify-center md:items-center h-full'>
+        <div className='flex flex-col gap-y-3 pt-40 md:pt-0 md:justify-center items-center h-full'>
         <motion.h1 initial={{x:-2000}} animate={{x:0}}
         transition={{duration:"2"
         }}
-        className='text-2xl md:text-6xl font-bold text-gray-100'>Hi, I 'm <span className="text-teal-400">Syed Shahmir Sultan</span></motion.h1>
+        className='text-[26px] md:text-6xl font-bold text-gray-100'>Hi, I 'm <span className="text-teal-400">Syed Shahmir Sultan</span></motion.h1>
         <motion.p  initial={{x:-2000}} animate={{x:0}}
         transition={{duration:"2"
         }}
         className="text-white font-bold text-xl md:text-5xl"
         > A Full-Stack Web Developer</motion.p>
+       <div className="flex gap-x-4 mt-2">
+        {
+            icons.map((item,index)=> {
+            return (
+                <motion.div key={index} initial={{x:-2000}} animate={{x:0}}
+                transition={{duration:"2"
+                }} className=' w-10 h-10 rounded-full flex items-center justify-center bg-gray-600 hover:bg-gray-400 cursor-ponter'>
+                    <Link href={item.url}>{item.icon}</Link>
+
+                </motion.div>
+            )
+            })
+        }
+     
+       </div> 
+
+       <motion.button initial={{x:-2000}} animate={{x:0}}
+                transition={{duration:"2"}} className=" mt-6 px-6 border-2 border-white hover:border-teal-300 py-3 text-gray-950 hover:bg-gradient-to-r from-teal-500  to-teal-900  hover:text-gray-200 text-md font-semibold bg-white rounded-md">Resume</motion.button>
         </div>
     );
 }
---
+++ app/Components/NavBar.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <section className="h-16 w-full bg-slate-900 flex items-center">
+           {/*Logo*/} 
+           <Link href="/"><p className="text-gray-100 text-2xl tracking-widest font-extrabold ml-8 uppercase">Shahmir</p></Link>
+        <div className="hidden md:flex ml-80 gap-x-16 items-center text-lg font-semibold ">
+        <Link href="" className=" text-gray-400 hover:text-white">About</Link>
+        <Link href="" className=" text-gray-400 hover:text-white">Skills</Link>
+        <Link href="" className=" text-gray-400 hover:text-white">Experience</Link>
+        <Link href="" className=" text-gray-400 hover:text-white">Contact</Link>
+
+
+        </div>
+        </section>
+    );
+}
+
+export default NavBar;
