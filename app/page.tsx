e0b6ea8065f891b7213046bfa08c5fe2aab23b41
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2024-07-29 07:51:47 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,14 @@ import Skills from './Components/Skills';
 import Experience from './Components/Experience';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
+import GoogleAnalytics from './Components/GoogleAnalytics';
+
 
 
 const Home = () => {
   return (
   <main className="min-h-screen w-full  bg-gradient-to-r from-black  to-gray-900">
+    <GoogleAnalytics/>
       <Hero/>
       <About/>
       <Skills/>
